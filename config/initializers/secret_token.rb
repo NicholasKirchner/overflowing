878239cf54d1fb3a74956ed32b17437eef413ebc
dbcapprentice <dbcapprentice@dbc.com>
2013-06-26 09:29:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Overflowing::Application.config.secret_token = 'c0b4654d4472410a98e13c5c8af7f2dc92c420d87b2b791f5103c9c6d59206c70a48936a5b361d641aa8bb44a7a21e3e485f5233fe40a75c1fef4847d6cb2f87'
