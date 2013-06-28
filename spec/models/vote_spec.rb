259dfed25f0759b637174aeebca478346136171c
Apprentice <apprentice@devbootcamp.com>
2013-06-27 19:02:50 -0700

    added factories, created test for votes and post

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,57 +2,35 @@ require 'spec_helper'
 
 describe Vote do
 
-  let(:user) {User.create(name: "maria", 
-                          email: "maria@gmail.com", 
-                          password: "password")}
+  let(:comment) {create(:comment)}
 
-  let(:post) {user.posts.create(title: "sweet gif", 
-                          image_url: "lolcats.com")}
+  let(:post) {comment.post}
 
-  let(:comment) {post.comments.create(content: "awesome post")}
+  let(:user) { create(:user) }
 
-  let(:comment_vote) {user.votes.create(votable_id: comment.id, 
-                          votable_type: "Comment", 
-                          value: 1)}
+  let!(:post_vote) {create(:vote, votable_id: post.id, votable_type: "Post", user_id: user.id)}
+  let(:post_vote_dup) {build(:vote, votable_id: post.id, votable_type: "Post", user_id: user.id)}
 
-  let(:comment_vote_dup) {user.votes.create(votable_id: comment.id, 
-                          votable_type: "Comment", 
-                          value: 1)}
+  let!(:comment_vote) {create(:vote, votable_id: comment.id, votable_type: "Comment", user_id: user.id)}
+  let(:comment_vote_dup) {build(:vote, votable_id: comment.id, votable_type: "Comment", user_id: user.id)}
+  
+  let(:invalid_vote) {build(:vote, votable_type: "Catss!!!")}
 
-  let(:post_vote) {user.votes.create(votable_id: comment.id, 
-                          votable_type: "Comment", 
-                          value: 1)}
-
-  let(:post_vote_dup) {user.votes.create(votable_id: comment.id, 
-                          votable_type: "Comment", 
-                          value: 1)}
-
-  let(:invalid_vote) {Vote.create(votable_id: comment.id, 
-                                 votable_type: "Catss!!!", 
-                                 value: 1)}
 
 
   it "should not allow user vote on the same comment more than once" do
-    comment_vote
-    expect(comment_vote_dup.id).to be_nil
+    expect(comment_vote_dup).to_not be_valid
   end
 
   it "should not allow user vote on the same post more than once" do
-    post_vote
-    expect(post_vote_dup.id).to be_nil
+    expect(post_vote_dup).to_not be_valid
   end
 
   it "should not allow voteable types other than Post and Comment" do
-    invalid_vote.valid?
-    expect(invalid_vote.errors.full_messages.first).to be {"Votable type is invalid"}
-  end
-
-  it "should have a value" do
-    expect(post_vote.value).to_not be_nil 
+    invalid_vote.save  
+    expect(invalid_vote.errors.full_messages).to include "Votable type is invalid"
   end
 
   
-
-
 
 end
