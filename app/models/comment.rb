159a1642d1f3c8f44266c3fe1f8360bdffc57d1f
Tej Singh <wctej89@gmail.com>
2013-06-29 21:00:44 -0700

    added votes on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,14 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
-  belongs_to :user
-  attr_accessible :content
-  has_many :votes, :as => :votable
-  
-  validates :content, presence: true
+	belongs_to :post
+	belongs_to :user
+	attr_accessible :content
+	has_many :votes, :as => :votable
+	
+	validates :content, presence: true
+
+	def total_votes
+    # post = Post.find(post_id)
+    return 0 if votes.empty?
+    votes.inject(0) { |sum, vote| sum + vote.value}
+  end
 end
