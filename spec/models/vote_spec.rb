e2fe4e241b139f2ef90e7d68c3296ae39d064891
Tej Singh <wctej89@gmail.com>
2013-08-14 21:37:34 -0700

    Vote model spec done

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe Vote do
-
-  let(:comment) {create(:comment)}
-
-  let(:post) {comment.post}
-
+  let(:vote) { FactoryGirl.create(:vote) }
+  let(:comment) { create(:comment) }
+  let(:post) { comment.post }
   let(:user) { create(:user) }
+
+  subject { vote }
+    it { should respond_to :user }
+    it { should respond_to :value }
 
   let!(:post_vote) {create(:vote, votable_id: post.id, votable_type: "Post", user_id: user.id)}
   let(:post_vote_dup) {build(:vote, votable_id: post.id, votable_type: "Post", user_id: user.id)}
@@ -15,8 +17,6 @@ describe Vote do
   let(:comment_vote_dup) {build(:vote, votable_id: comment.id, votable_type: "Comment", user_id: user.id)}
   
   let(:invalid_vote) {build(:vote, votable_type: "Catss!!!")}
-
-
 
   it "should not allow user vote on the same comment more than once" do
     expect(comment_vote_dup).to_not be_valid
@@ -30,7 +30,4 @@ describe Vote do
     invalid_vote.save  
     expect(invalid_vote.errors.full_messages).to include "Votable type is invalid"
   end
-
-  
-
 end
